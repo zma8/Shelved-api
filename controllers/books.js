b1a3a3b98bf1f0cb559580fa255559893ab0dea3
zma8 <zainabmoosa880@gmail.com>
2026-05-03 08:30:36 +0300

    add book controller with get,post,put

---
+++ controllers/books.js
@@ -0,0 +1,55 @@
+const express=require('express');
+const Book=require('../models/Book');
+
+const router=express.Router();
+
+router.get('/',async(req,res)=>{
+    try{
+        const books=await Book.find({userId:req.user._id}).sort({createdAt:-1});
+        res.json(books);
+    }catch(err){
+        res.status(500).json({ err: 'Something went wrong' });
+    }
+});
+
+router.post('/',async (req,res)=>{
+    try{
+        const newBook=await Book.create({
+            title:req.body.title,
+            status:req.body.status || 'Maybe today?',
+            note:req.body.note || '',
+            mood:req.body.mood || '',
+            userId:req.user._id,
+        });
+        res.status(201).json(newBook);
+    }catch(err){
+        res.status(500).json({ err: 'Something went wrong' });
+    }
+});
+
+router.put('/:id',async (req,res)=>{
+    try{
+        const book=await Book.findOne({_id:req.params.id,userId:req.user._id});
+
+        if(!book){
+            return res.status(404).json({err:'Book not found'}); 
+        }
+
+        if(req.body.status && req.body.status !==book.status){
+            req.body.lastOpened=new Date();
+        }
+
+        const updateBook=await Book.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        );
+
+        res.json(updateBook);
+
+    }catch(err){
+        res.status(500).json({ err: 'Something went wrong' });
+    }
+})
+
+module.exports=router;
